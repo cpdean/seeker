a932e6e985de6205573b0dc849a6405be15795eb
Conrad Dean <conrad.p.dean@gmail.com>
2016-01-07 23:42:53 -0500

    simple tests

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,2 +1,36 @@
+from __future__ import print_function
+import re
+
+
+class CannotFindIdentifier(Exception):
+    pass
+
+
+class SearchError(Exception):
+    pass
+
+
+def find_location(source, line, col, identifier):
+    """
+    assume source is a string
+
+    maybe should be a file
+    """
+    id_regex = re.compile(identifier + r"\s+=")
+    defs = []
+    lines = source.split("\n")
+    for ix, line in enumerate(lines):
+        found = id_regex.search(line)
+        if found:
+            defs.append((ix, found.start()))
+    if len(defs) == 0:
+        raise CannotFindIdentifier(
+            "could not find {} in here".format(identifier))
+    if len(defs) > 1:
+        raise SearchError(
+            "too many matches: {}".format([lines[r] for r, c in defs]))
+    return defs[0]
+
+
 def main():
-    print "sup"
+    print("sup")
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,6 +1,7 @@
 import seeker
+import pytest
 
-src = """
+regular = """
 import List
 import String
 
@@ -13,9 +14,45 @@ b input = String.join "." (splitter input)
 
 
 def test_tokens_wow():
-    assert type(src) == str
+    assert type(regular) == str
 
 
 def test_find_origin():
-    location = seeker.find_location(src, 8, 27, "splitter")
+    location = seeker.find_location(regular, 8, 27, "splitter")
     assert location == (5, 0)
+
+
+too_many = """
+import List
+import String
+
+splitter : String -> List String
+splitter = String.split " "
+
+splitter : String -> List String
+splitter = String.split " "
+
+b : String -> String
+b input = String.join "." (splitter input)
+"""
+
+
+def test_too_many_should_fail():
+    with pytest.raises(seeker.SearchError):
+        location = seeker.find_location(too_many, 10, 27, "splitter")
+        assert location == (5, 0)
+
+
+missing = """
+import List
+import String
+
+b : String -> String
+b input = String.join "." (splitter input)
+"""
+
+
+def test_simple_missing():
+    with pytest.raises(seeker.CannotFindIdentifier):
+        location = seeker.find_location(missing, 4, 27, "splitter")
+        assert location == (5, 0)
